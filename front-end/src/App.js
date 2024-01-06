42d0bee8c211631e0e9ee90516c5b8c1e5177857
yousef_walid <yw012276@gmail.com>
2024-01-06 21:15:52 +0200

    adding product and unil parts in our project

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Products from './components/products/Products.js'
+import Whoarewe from './components/util/Whoarewe.jsx'
+import Whychooseus from './components/util/Whychooseus.jsx'
+import Ourteam from './components/util/Ourteam.jsx'
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <Products />
+      <Whoarewe/>
+      <Whychooseus/>
+      <Ourteam/>
+    </>
+  );
 }
 
 export default App
