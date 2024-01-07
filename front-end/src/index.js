551850f71ed0af81f2743eeeb9080524f2b919a6
yousef_walid <yw012276@gmail.com>
2024-01-07 11:33:00 +0200

    creating routes

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,10 +5,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import App from './App';
 import "./App.css"
+import {BrowserRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
