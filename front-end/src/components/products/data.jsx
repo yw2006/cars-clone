619efec0b0e5e3be2b11c05b2d45f4a15fb3d629
AnasHanafy <nsmhmf@gmail.com>
2024-01-07 20:42:58 +0200

    car names edited

--- front-end/src/components/products/data.jsx
+++ front-end/src/components/products/data.jsx
@@ -23,7 +23,7 @@ export const responsive = {
 export const stock = [
     {
         id: '1',
-        name: 'Ford Transit – 2021',
+        name: 'Toyota Supra– 1995',
         image: 'https://i.pinimg.com/236x/09/e3/70/09e37078bc92d14515a91d1a5260e8ba.jpg',
         price: '$22,000',
         miles: '2500 miles',
@@ -35,7 +35,7 @@ export const stock = [
     },
     {
         id: '2',
-        name: 'New GLC – 2023',
+        name: 'Ferrari laFerrari– 2014',
         image: 'https://i.pinimg.com/236x/25/9b/07/259b072becf54e2a8de19814c1277d7f.jpg',
         price: '$95,000',
         miles: '3000 miles',
@@ -46,7 +46,7 @@ export const stock = [
 
     },
     {
-        name: 'Audi A6 3.5 – New',
+        name: 'Porshe Cayman– New',
         image: 'https://i.pinimg.com/564x/ea/58/6c/ea586c6058797475fa2025c49183d415.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -58,7 +58,7 @@ export const stock = [
     },
     {
         id: '3',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW X6 – New',
         image: 'https://i.pinimg.com/236x/91/20/5e/91205e32740069b054461f881e3003d2.jpg',
         price: '$58,000',
         miles: '2300miles',
@@ -70,7 +70,7 @@ export const stock = [
     },
     {
         id: '4',
-        name: 'Audi A6 3.5 – New',
+        name: 'Mazda RX7 – New',
         image: 'https://i.pinimg.com/236x/6a/b1/7d/6ab17d14652614e1bbc5cb573cd0570b.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -82,7 +82,7 @@ export const stock = [
     },
     {
         id: '5',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/32/58/1b/32581ba18f56fe098b96af32918d47ff.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -94,7 +94,7 @@ export const stock = [
     },
     {
         id: '6',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW – New',
         image: 'https://i.pinimg.com/236x/ae/d2/6c/aed26cd252fa99739aba563239c89a7a.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -106,7 +106,7 @@ export const stock = [
     },
     {
         id: '7',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/31/61/3d/31613dbffd8c9426ff6ad1c41e516b82.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -118,7 +118,7 @@ export const stock = [
     },
     {
         id: '8',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/31/61/3d/31613dbffd8c9426ff6ad1c41e516b82.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -130,7 +130,7 @@ export const stock = [
     },
     {
         id: '9',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/31/61/3d/31613dbffd8c9426ff6ad1c41e516b82.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -142,7 +142,7 @@ export const stock = [
     },
     {
         id: '10',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/32/58/1b/32581ba18f56fe098b96af32918d47ff.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -154,7 +154,7 @@ export const stock = [
     },
     {
         id: '11',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW e36 – New',
         image: 'https://i.pinimg.com/236x/2d/c5/eb/2dc5eb014df05e5e0276b7abfaf282f7.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -178,7 +178,7 @@ export const stock = [
     },
     {
         id: '13',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW M8 – New',
         image: 'https://i.pinimg.com/236x/0e/9f/f8/0e9ff88b7d89f6a64460b67870f2a4dc.jpg',
         price: '$58,000',
         miles: '2300 miles',
@@ -190,7 +190,7 @@ export const stock = [
     },
     {
         id: '14',
-        name: 'Audi A6 3.5 – New',
+        name: 'BMW M2 F22 – New',
         image: 'https://i.pinimg.com/236x/6f/85/72/6f85728b4d1fb853f7fbfa82184e1403.jpg',
         price: '$58,000',
         miles: '2300 miles',
