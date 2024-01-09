068ecc5f9235fae8e7db5e97a78befc7b1af02c9
yousef_walid <yw012276@gmail.com>
2024-01-09 17:18:06 +0200

    nothing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/details/:id' element={<Details/>} />
+      <Route path='/details/:id' element={<Details/> } />
     </Routes>
   );
 
--- front-end/src/components/products/Product.jsx
+++ front-end/src/components/products/Product.jsx
@@ -44,7 +44,7 @@ export function Product(props) {
             </ul>
             <div className="card-body ">
                 <span className="m-2 button">{props.price}</span>
-                <a href={`/CardDetails/${props.id}`} className=" card-link btn " style={{ color: 'white' }}>view details <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                <a href={`/details/${props.id}`} className=" card-link btn " style={{ color: 'white' }}>view details <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
                 </svg></a>
             </div>
