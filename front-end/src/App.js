46d8b135df583d41c28832c668c35cb89b097e41
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-01-09 20:57:50 +0200

    added login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import Home from './pages/Home';
 import Details from './pages/Details';
+import Login from './components/Login';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/details/:id' element={<Details/>} />
+      <Route path='/login' element={<Login/>} />
+
     </Routes>
   );
 
